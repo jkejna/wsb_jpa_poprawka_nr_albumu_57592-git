15099e020cf8095c7e3f205031a87a0d1e530f2b
Jak <j.kejna97@o2.pl>
2024-06-05 20:58:26 +0200

    Initial commit

---
+++ wsb-jpa1/src/main/java/com/capgemini/wsb/persistence/dao/impl/DoctorDaoImpl.java
@@ -0,0 +1,32 @@
+package com.capgemini.wsb.persistence.dao.impl;
+
+import com.capgemini.wsb.persistence.dao.DoctorDao;
+import com.capgemini.wsb.persistence.entity.DoctorEntity;
+
+import com.capgemini.wsb.persistence.enums.Specialization;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.Query;
+import java.util.List;
+
+@Repository
+public class DoctorDaoImpl extends AbstractDao<DoctorEntity, Long> implements DoctorDao {
+    @Override
+    public List<DoctorEntity> findBySpecialization(Specialization specialization) {
+        Query query = entityManager.createQuery("select d from DoctorEntity d " + "where d.specialization = :spec").setParameter("spec", specialization);
+
+        return query.getResultList();
+    }
+
+    @Override
+    public long countNumOfVisitsWithPatient(String docFirstName, String docLastName, String patientFirstName, String patientLastName) {
+        Query query = entityManager.createQuery("select d from DoctorEntity d " +
+                "join d.visits v " + "where d.firstName = :docFirstName and d.lastName = :docLastName " +
+                "and v.patient.firstName = :patientFirstName and v.patient.lastName = :patientLastName") //
+                .setParameter("docFirstName", docFirstName) //
+                .setParameter("docLastName", docLastName)
+                .setParameter("patientFirstName", patientFirstName)
+                .setParameter("patientLastName", patientLastName);
+        return query.getResultList().size();
+    }
+}
---
+++ wsb-jpa1/src/main/java/com/capgemini/wsb/persistence/dao/impl/PatientDaoImpl.java
@@ -0,0 +1,54 @@
+package com.capgemini.wsb.persistence.dao.impl;
+
+import com.capgemini.wsb.persistence.dao.PatientDao;
+import com.capgemini.wsb.persistence.entity.PatientEntity;
+
+import com.capgemini.wsb.persistence.enums.TreatmentType;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.Query;
+import java.util.List;
+
+@Repository
+public class PatientDaoImpl extends AbstractDao<PatientEntity, Long> implements PatientDao {
+
+
+    @Override
+    public List<PatientEntity> findByDoctor(String firstName, String lastName) {
+        Query query = entityManager.createQuery("select p from PatientEntity p " +
+                "join p.visits v " + "where v.doctor.firstName = :firstName and v.doctor.lastName = :lastName") //
+                .setParameter("firstName", firstName) //
+                .setParameter("lastName", lastName);
+
+        return query.getResultList();
+
+    }
+
+    @Override
+    public List<PatientEntity> findPatientsHavingTreatmentType(TreatmentType treatmentType) {
+        Query query = entityManager.createQuery("select distinct p from PatientEntity p " +
+                "join p.visits v " + "join v.medicalTreatments mt " +
+                "where mt.type = :treatmentType") //
+                .setParameter("treatmentType", treatmentType);
+
+        return query.getResultList();
+    }
+
+    @Override
+    public List<PatientEntity> findPatientsSharingSameLocationWithDoc(String firstName, String lastName) {
+        Query query = entityManager.createQuery("select p from PatientEntity p " +
+                "join p.addresses a " + "join a.doctors d " +
+                "where d.firstName = :docFirstName and d.lastName = :docLastName") //
+                .setParameter("docFirstName", firstName) //
+                .setParameter("docLastName", lastName);
+
+        return query.getResultList();
+    }
+
+    @Override
+    public List<PatientEntity> findPatientsWithoutLocation() {
+        Query query = entityManager.createQuery("select p from PatientEntity p where size(p.addresses) = 0");
+
+        return query.getResultList();
+    }
+}
